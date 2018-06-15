52b37b5bd560e0bcf3737e907818233d2b828816
Breno Ramalho Lemes <breno@br-lemes.net>
2018-06-14 22:47:13 -0400

    Interação entre a página principal e a de pedidos por anchor ao invés de query string. O Formspree não aceita URL de origem com query string.

--- script.js
+++ script.js
@@ -1,39 +1,30 @@
-/* queryData Created by Kate Morley */
-
-function queryData() {
-	var qData = { }, qString = location.search ? location.search : '';
-	if (qString.charAt(0) == '?') qString = qString.substring(1);
-	if (qString.length > 0) {
-		qString = qString.replace(/\+/g, ' ');
-		var qComponents = qString.split(/[&;]/g);
-		for (var index = 0; index < qComponents.length; index ++) {
-			var keyValuePair = qComponents[index].split('=');
-			var key          = decodeURIComponent(keyValuePair[0]);
-			var value        = keyValuePair.length > 1
-				? decodeURIComponent(keyValuePair[1]) : '';
-			qData[key] = value;
-		}
-	}
-	return qData;
-}
+var moldura = [
+    "", // skip index 0
+    "Moldura lisa escura - R$ 78,00",
+    "Moldura com desenhos - R$ 81,00",
+    "Moldura em relevo simples - R$ 125,00",
+    "Moldura entalhada simples - R$ 140,00",
+    "Moldura entalhada - R$ 215,00",
+    "Moldura com detalhes - R$ 250,00"
+];
 
 var view, bar;
 
-window.onload = function() {
-	var qData = queryData();
-    if (qData.modelo && qData.desc) {
+window.onload = function () {
+    var i = document.URL.split('#')[1];
+    if (i) {
         view = document.getElementById("moldura-view");
         bar = document.getElementById("moldura-info");
-        view.firstElementChild.src = "imagens/moldura" + qData.modelo + ".jpg";
+        view.firstElementChild.src = "imagens/moldura" + i + ".jpg";
         var p = bar.firstElementChild;
         bar.style.backgroundColor = "#2ECC40";
-        document.getElementById("modelo").value = qData.modelo;
-        p.innerHTML = qData.desc;
-        p.onmouseover = function() {
+        document.getElementById("modelo").value = i;
+        p.innerHTML = moldura[i];
+        p.onmouseover = function () {
             view.style.visibility = "visible";
-        }
-        p.onmouseout = function() {
+        };
+        p.onmouseout = function () {
             view.style.visibility = "hidden";
-        }
+        };
     }
-}
+};
